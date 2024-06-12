fadaca61ecedac79c52ae391abca1320324b3b96
Seu nome <yago_bohmcruz@hotmail.com>
2024-06-12 12:33:37 -0300

    Cria a página Home :house:

---
+++ src/pages/Home.tsx
@@ -0,0 +1,14 @@
+import "./Home.scss";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Header</h1>
+      <section>
+      <h1>Table</h1>
+      </section>
+    </div>
+  );
+};
+
+export default Home;
